44b9c2f501c7aea2400cd93ac2eeb76b8092d8fd
zhengwentaozwt <>
2021-03-03 18:37:06 +0800

    测试

--- AndroidProject/app/src/main/java/com/example/findit/RegisterActivity.java
+++ AndroidProject/app/src/main/java/com/example/findit/RegisterActivity.java
@@ -62,7 +62,6 @@ public class RegisterActivity extends AppCompatActivity {
         setListeners();
 
 
-
         handler=new Handler(){
             @Override
             public void handleMessage(Message msg) {
